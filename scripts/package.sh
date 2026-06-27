b7b8be209c7dd7d02dc868e087b18896075bf1f5
Hector Diaz <hdiazc@live.com>
2026-06-27 12:50:34 -0600

    ci: add release packaging script and workflow

---
+++ scripts/package.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+VERSION="${1:?usage: package.sh VERSION}"
+STAGE="mural-${VERSION}"
+
+rm -rf "$STAGE"
+mkdir -p "$STAGE/data" "$STAGE/bin"
+cp dist/mural.js "$STAGE/"
+cp data/dev.muy.Mural.desktop data/dev.muy.Mural.metainfo.xml data/dev.muy.Mural.svg "$STAGE/data/"
+cp bin/mural "$STAGE/bin/"
+cp README.md "$STAGE/"
+
+tar czf "${STAGE}.tar.gz" "$STAGE"
+rm -rf "$STAGE"
